f1c42efdd1fc9e9777b301718fc6de01e9dbd8f7
yusuf7sui <yusuf.akb07@gmail.com>
2025-08-04 08:19:39 +0200

    Add entry pages.
    Added entry pages that display title and content
    of entries.
    Implemented an error page which is shown when
    a requested entry doesn't exist.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    entry = util.get_entry(title)
+    if entry is not None:
+        html_content = markdown2.markdown(entry)
+        return render(request, "encyclopedia/title.html",{
+        "title": title,
+        "content": html_content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    
