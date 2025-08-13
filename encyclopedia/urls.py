f90a11edf256b44c7589ff196a7d7b71ae48e466
yusuf7sui <yusuf.akb07@gmail.com>
2025-08-13 17:05:33 +0200

    Add the create new page function
    Adjusted layout.html to redirect to the
    create entry page.
    Added a form to create a new entry page.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("new-page", views.new_page, name="new-page"),
     path("<str:entry>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,11 +7,18 @@ import markdown2
 
 
 def index(request):
+    """
+    Renders the homepage with a list of all encyclopedia entries.
+    """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, entry):
+    """
+    Renders a specific entry page.
+    If the entry doesn't exist, an error page will be shown.
+    """
     entry = util.get_entry(entry)
     if entry is not None:
         html_content = markdown2.markdown(entry)
@@ -25,6 +32,10 @@ def entry(request, entry):
         })
     
 def search(request):
+    """
+    If the query matches an existing entry, the user is redirected to that page.
+    Otherwise, a search page is rendered, which includes all entries that contain the query as a substring.
+    """
     result_entries = util.list_entries()
     query = request.GET.get("q", "")        
     result_entries = list(filter(lambda x: query.lower() in x.lower(), result_entries))
@@ -33,3 +44,17 @@ def search(request):
     return render(request, "encyclopedia/search-results.html", {
         "results": result_entries
     })
+
+def new_page(request):
+    """
+    If the create click button is clicked the create new page is rendered.
+    When the form is submitted via POST with a title and content,
+    the entry will be saved and the user is redirected to the new entry page.
+    """
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("entry", kwargs={"entry": title}))
+    return render(request, "encyclopedia/new-page.html")
+    
